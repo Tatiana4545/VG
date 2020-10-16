75cdc3e3dac7b713f99b04c47a6bb3d32ac15b20
Tatiana4545 <tatianalobynets@gmailcom>
2020-10-16 19:11:03 +1000

    $

--- main.py
+++ main.py
@@ -3,25 +3,63 @@ import random
 import sys
 import os
 import time
+from colors import COLOR
 
-os.environ["SDL_VIDEO_POS"] ="0,0"
+os.environ["SDL_VIDEO_WINDOW_POS"] = "0,0"
 # os.environ["SDL_VIDEO_CENTERED"] = "1
 pygame.init()
 
 info = pygame.display.Info()
-WIDTH_WIN, HEIGFT_WIN = Info.current_w * 100//125, Info.current_h 100//150
-print(info.curren_w, info.curren)
-print(WIDTH_WIN, HEIGFT_WIN)
+WIDTH_WIN, HEIGFT_WIN = info.current_w, info.current_h
+# print(info.curren_w, info.curren)
+# print(WIDTH_WIN, HEIGFT_WIN)
 
+path = os.path.dirname(os.path.abspath(__file__))
+
+saturn = os.path.join(path, "6s.png")
+pygame.display.set_icon(pygame.image.load(saturn))
+pygame.mouse.set_visible(False)
 screen = pygame.display.set_mode((WIDTH_WIN, HEIGFT_WIN))
 pygame.mouse.set_visible(False)
 
 FPS = 60
 clock = pygame.time.Clock()
+NUMBER_OF_STARS = 200
+NIGHT_BG_COLOR = (5, 0, 50)
+
+
+class Stars(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.speed = random.randint(1,2)
+        self.size = random.randrange(1,3)
+        self.pos = random.randrange(WIDTH_WIN), random.randrange(HEIGFT_WIN)
+        self.image = pygame.Surface((self.size * 2, self.size * 2))
+        pygame.draw.circle(self.image, pygame.Color(
+            random.choice(COLOR[238:262])), [self.size, self.size], self.size)
+        self.rect = self.image.get_rect(center=self.pos)
+
+
+sprites = pygame.sprite.LayeredUpdates()
+for _ in range(NUMBER_OF_STARS):
+    stars = Stars()
+    sprites.add(stars, layer=0)
 
 run = True
-while run
-    for  e  in pygame.event.get()
-        if e.tyre == pygame.QUIT or e.tyre ==pygame.KEYDOWN and e.key == pygame.K_ESCAP:
-        run = False
-        
+while run:
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT or e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
+            run = False
+
+    sprites.update()
+    screen.fill(NIGHT_BG_COLOR)
+    sprites.draw(screen)
+    pygame.display.update()
+            
+
+
+
+
+
+
+
